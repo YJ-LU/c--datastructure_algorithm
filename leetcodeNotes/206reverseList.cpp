3718cf7e94fbc8a2e6a3db064407fde69a92237e
YJ-LU <51699449+YJ-LU@users.noreply.github.com>
2022-04-17 16:03:16 +0800

    Update 206reverseList.cpp

--- leetcodeNotes/206reverseList.cpp
+++ leetcodeNotes/206reverseList.cpp
@@ -1,1 +1,34 @@
-public class ListNode{}
+//
+// Created by apple on 2022/4/16.
+//
+//Definition for singly-linked list
+
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(): val(0), next(nullptr){}
+    ListNode(int x): val(x), next(nullptr){}
+    ListNode(int x, ListNode* next): val(x), next(next){}
+};
+
+class Solution{
+    public:
+        ListNode* reverseList(ListNode* head){
+            // set a previous pointer
+            ListNode* pre = nullptr;
+
+            ListNode* cur = head;
+
+            while (cur){
+                //set a tem pointer for traverse
+                ListNode *tem = cur->next;
+                // reverse
+                cur->next = pre;
+                pre = cur;
+                cur = tem;
+            }
+            return pre;
+    }
+};
+// time 8ms
+// memory 8mb
