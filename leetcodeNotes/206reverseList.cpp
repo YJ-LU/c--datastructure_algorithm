0f8704fd72c268f0b400cff7dd1cec6b90e7d547
YJ-LU <51699449+YJ-LU@users.noreply.github.com>
2022-04-17 22:50:56 +0800

    Update 206reverseList.cpp

--- leetcodeNotes/206reverseList.cpp
+++ leetcodeNotes/206reverseList.cpp
@@ -1,7 +1,9 @@
 //
-// Created by apple on 2022/4/16.
+// Created by YJ on 2022/4/16.
 //
 //Definition for singly-linked list
+#include <iostream>
+using namespace std;
 
 struct ListNode{
     int val;
@@ -11,8 +13,11 @@ struct ListNode{
     ListNode(int x, ListNode* next): val(x), next(next){}
 };
 
+//reverse actually implement by reversing the direction of the signal pointer
 class Solution{
     public:
+        // time 8ms
+        // memory 8 MB
         ListNode* reverseList(ListNode* head){
             // set a previous pointer
             ListNode* pre = nullptr;
@@ -22,13 +27,41 @@ class Solution{
             while (cur){
                 //set a tem pointer for traverse
                 ListNode *tem = cur->next;
-                // reverse
+                // reverse point
                 cur->next = pre;
                 pre = cur;
                 cur = tem;
             }
+            //return as new head
             return pre;
-    }
+        }
+
+        // time 8ms
+        // memory 8.4 MB
+        ListNode* reverseList2(ListNode* head){
+            //base situation
+            if(!head && !head->next)
+                return head;
+            //recursive
+            ListNode* rHead = reverseList2(head->next);
+            //reverse the pointer
+            head->next->next = head;
+            //after reverse, the next pointer is null
+            head->next = nullptr;
+            //return as new head
+            return rHead;
+        }
 };
-// time 8ms
-// memory 8mb
+
+int main() {
+    ListNode* head;
+    Solution s;
+    for(int i=1; i<10; i++){
+        head->val = i;
+        head = head->next;
+    }
+
+    cout << "reverse1 is: " << s.reverseList(head) << endl;
+    return 0;
+}
+
